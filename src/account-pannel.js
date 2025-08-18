8c200e5888c3f8dbb7c99c2109617504d5260c16
Rishab Singh <rishabsiingh@outlook.com>
2025-08-18 15:03:31 +0530

    added dark mode

--- src/account-pannel.js
+++ src/account-pannel.js
@@ -1,6 +1,23 @@
 const accBtn = document.querySelector(".account-circle");
 const  float = document.querySelector(".account-floating-bar");
+const drkBtn = document.querySelector(".dark-button")
+const drkTxt = document.querySelector(".dark-mode-text")
+const drkimg = document.querySelector(".dark-mode-image")
 
 accBtn.addEventListener("click", () => {
- float.classList.toggle("hidden");
+  float.classList.toggle("hidden");
 });
+
+drkBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+
+  if(document.documentElement.classList.contains("dark")) {
+    drkTxt.textContent = "Light Mode";
+    drkimg.src = "./src/images/light_mode.svg"
+  }
+  else {
+    drkTxt.textContent = "Dark Mode";
+    drkimg.src = "./src/images/dark_mode.svg"
+  }
+
+})
