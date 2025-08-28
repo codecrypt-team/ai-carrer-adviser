4b4b77ef48df9f7304fa2d2caf8dea25a53a6e82
Rishab Singh <rishabsiingh@outlook.com>
2025-08-28 12:49:10 +0530

    add animation everywhere

--- src/animations/index-animation.js
+++ src/animations/index-animation.js
@@ -1,3 +1,4 @@
+import { typeWriter } from '../utils/typewriter.js';
 document.addEventListener('DOMContentLoaded', () => {
   const animated = document.querySelectorAll(".animated");
 
@@ -6,4 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
        el.classList.remove("opacity-0", "translate-y-5")
     }, index * 200)
   });
+  const myText = "Welcome back, Ninja Hattori👋"
+  typeWriter(".welcome-text-container", myText, 50);
 });
